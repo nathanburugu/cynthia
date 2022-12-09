d28c6e186100c2921f67717ca644fac3fe5a6473
nathan <57700936+nathanburugu@users.noreply.github.com>
2022-12-09 21:30:03 +0300

    Add files via upload

---
+++ PropertyList.jsx
@@ -0,0 +1,68 @@
+/** @format */
+
+import React from 'react';
+import './propertyList.css';
+
+const PropertyList = () => {
+  return (
+    <div className="pList">
+      <div className="pListItem">
+        <img
+          src="https://images.unsplash.com/photo-1560564029-6eb181a872c4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
+          alt="picha kali"
+          className="pListImg"
+        />
+        <div className="pListTitles">
+          <h1>Hotels</h1>
+          <h2>233 Hotels</h2>
+        </div>
+      </div>
+      <div className="pListItem">
+        <img
+          src="https://images.unsplash.com/photo-1560564029-6eb181a872c4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
+          alt="picha kali"
+          className="pListImg"
+        />
+        <div className="pListTitles">
+          <h1>Appartments</h1>
+          <h2>2331 Houses</h2>
+        </div>
+      </div>
+      <div className="pListItem">
+        <img
+          src="https://images.unsplash.com/photo-1560564029-6eb181a872c4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
+          alt="picha kali"
+          className="pListImg"
+        />
+        <div className="pListTitles">
+          <h1>Resorts</h1>
+          <h2>233 Hotels</h2>
+        </div>
+      </div>
+      <div className="pListItem">
+        <img
+          src="https://images.unsplash.com/photo-1560564029-6eb181a872c4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
+          alt="picha kali"
+          className="pListImg"
+        />
+        <div className="pListTitles">
+          <h1>Cabins</h1>
+          <h2>233 Hotels</h2>
+        </div>
+      </div>
+      <div className="pListItem">
+        <img
+          src="https://images.unsplash.com/photo-1560564029-6eb181a872c4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
+          alt="picha kali"
+          className="pListImg"
+        />
+        <div className="pListTitles">
+          <h1>Villas</h1>
+          <h2>233 Hotels</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PropertyList;
